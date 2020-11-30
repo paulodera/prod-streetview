441bc3aae8a1e2fca09789223997c150d82ee608
Paul Odera <oderapaul7@gmail.com>
2020-11-30 23:32:51 +0300

    heroku setup

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
+postgres://ttxooertajrmek:6e39f1006003c3fc3a3c4bdabf361a723471f9b455893dc473ca639330a5afff@ec2-52-203-182-92.compute-1.amazonaws.com:5432/d1ge2urdv81f7t
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from app import app
 
-app.run(host="0.0.0.0", port=8000, debug=True)
+if __name__ == '__main__':
+    app.run()
 
