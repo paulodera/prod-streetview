7bcf1f964146b307fbad0adb393226b9b43fe897
Paul Odera <paul.odera@isobar.com>
2021-04-02 16:00:32 +0300

    build database migrations

--- application/__init__.py
+++ application/__init__.py
@@ -72,32 +72,32 @@ from application.treasure_mod.models import Treasure
 from application.player_mod.models import PlayerLeaderBoard
 from application.clue_mod.models import Clue
 from application.clue_mod.models import ClueOptions
-from application.auth_mod.controllers import auth_mod as auth_module
-from application.treasure_mod.controllers import treasure_mod as treasure_module
-from application.hunt_mod.controllers import hunt_mod as hunt_module
-from application.clue_mod.controllers import clue_mod as clue_module
-from application.player_mod.controllers import player_mod as player_module
+# from application.auth_mod.controllers import auth_mod as auth_module
+# from application.treasure_mod.controllers import treasure_mod as treasure_module
+# from application.hunt_mod.controllers import hunt_mod as hunt_module
+# from application.clue_mod.controllers import clue_mod as clue_module
+# from application.player_mod.controllers import player_mod as player_module
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    return Auth.get(user_id)
+# @login_manager.user_loader
+# def load_user(user_id):
+#     return Auth.get(user_id)
 
 
-@application.route('/', methods=['GET'])
-def start():
-    data = {
-        'treasure': Treasure.get_all(),
-        'leaderboard': PlayerLeaderBoard.get_player_rankings()
-    }
-    return render_template('streetview/start.html', data=data)
+# @application.route('/', methods=['GET'])
+# def start():
+#     data = {
+#         'treasure': Treasure.get_all(),
+#         'leaderboard': PlayerLeaderBoard.get_player_rankings()
+#     }
+#     return render_template('streetview/start.html', data=data)
 
 
-application.register_blueprint(auth_module)
-application.register_blueprint(treasure_module)
-application.register_blueprint(hunt_module)
-application.register_blueprint(clue_module)
-application.register_blueprint(player_module)
+# application.register_blueprint(auth_module)
+# application.register_blueprint(treasure_module)
+# application.register_blueprint(hunt_module)
+# application.register_blueprint(clue_module)
+# application.register_blueprint(player_module)
 
 
 db.create_all()
